8297f4482011bf02ad72d351950dde6e2d32f990
Bhavan A <BAG9@eurofins.com>
2019-08-03 15:46:16 +0530

    added menu

--- bootstrap-dunzo/src/app/app-routing.module.ts
+++ bootstrap-dunzo/src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashBoardComponent } from './feature/dash-board/dash-board.component';
+import { UploadImageComponent } from './feature/upload-image/upload-image.component';
+import { AboutComponent } from './feature/about/about.component';
+import { ContactComponent } from './feature/contact/contact.component';
 
 const routes: Routes = [
   {
@@ -11,6 +14,18 @@ const routes: Routes = [
   {
     path: 'dash-board',
     component: DashBoardComponent
+  },
+  {
+    path: 'upload-image',
+    component: UploadImageComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
   }
 ];
 
--- bootstrap-dunzo/src/app/app.module.ts
+++ bootstrap-dunzo/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './common/footer/footer.component';
 import { HeaderComponent } from './common/header/header.component';
 import { DashBoardComponent } from './feature/dash-board/dash-board.component';
+import { UploadImageComponent } from './feature/upload-image/upload-image.component';
+import { AboutComponent } from './feature/about/about.component';
+import { ContactComponent } from './feature/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    DashBoardComponent
+    DashBoardComponent,
+    UploadImageComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
